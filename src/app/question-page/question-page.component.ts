4a1742e45066da4457037943253fe6280117d09f
geffen <geffenm@mail.tau.ac.il>
2018-12-04 17:55:58 +0200

    create question-page

---
+++ src/app/question-page/question-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-page',
+  templateUrl: './question-page.component.html',
+  styleUrls: ['./question-page.component.scss']
+})
+export class QuestionPageComponent implements OnInit {
+  chips: string[] = ['Data Structures'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
