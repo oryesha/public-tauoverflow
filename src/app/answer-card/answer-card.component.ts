334910d57c743d499c232b4ffa96a1cf7b82daa7
geffen <geffenm@mail.tau.ac.il>
2018-12-04 18:26:20 +0200

    add answer-card to question-page

--- src/app/answer-card/answer-card.component.ts
+++ src/app/answer-card/answer-card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./answer-card.component.scss']
 })
 export class AnswerCardComponent implements OnInit {
-
+  date: Date = new Date(2018, 9, 14);
   constructor() { }
 
   ngOnInit() {
