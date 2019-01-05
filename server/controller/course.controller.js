7de016cec55b8ca0b4203f065433319807417fb8
orilicht <orilicht10@gmail.com>
2019-01-05 13:19:06 +0200

    Merge branch 'master' of https://github.com/natiyo/TauOverflow into server
    # Conflicts:
    #	server/controller/course.controller.js

--- server/controller/course.controller.js
+++ server/controller/course.controller.js
@@ -1,4 +1,4 @@
-let CourseService = require('../services/course.service')
+let CourseService = require('../services/course.service');
 
 exports.getAllCourses = async function(req, res) {
   try{
@@ -25,7 +25,7 @@ exports.createCourse = async function(req, res) {
     }catch(e){
       return res.status(400).json({status: 400, message: "Course Creation was Unsuccesfull"})
     }
-}
+};
 
 exports.getCourse = async function(req, res) {
   const courseNum = req.params.courseId;
