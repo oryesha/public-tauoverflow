8c27b9653c3ccbd98dc8f430a343484afaed5a53
orilStudent <44923364+orilStudent@users.noreply.github.com>
2019-01-07 12:46:55 +0200

    Update query.controller.js

--- server/controller/query.controller.js
+++ server/controller/query.controller.js
@@ -1,11 +1,11 @@
-// let AnswerService = require('../services/answer.service');
+// let QueryService = require('../services/query.service');
 
 
 exports.getQueryResults = async function(req,res){
   try{
-    //let answers = await AnswerService.getAllAnswers({});
+    let questions = await QueryService.getQuestionsFromQuery(req.query);
     console.log(req.query);
-    return res.status(200).json({status: 200, data: 'wow', message: "Succesfully query Recieved"});
+    return res.status(200).json({status: 200, data: questions, message: "Succesfully query Recieved"});
   }catch(e){
     return res.status(400).json({status: 400, message: "WOW " + e.message});
   }
