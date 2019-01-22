b231c40a4f94123a742b64c1efee4eb8e60c5c92
OR <OR@PC>
2019-01-22 16:16:22 +0200

    fix query filter

--- server/services/query.service.js
+++ server/services/query.service.js
@@ -2,20 +2,20 @@ let UserProfile = require('../models/user-profile.model');
 let Answer = require('../models/answer.model');
 let Question = require('../models/question.model');
 let Course = require('../models/course.model');
+let serviceHelper = require('../services/serviceHelper');
 
 _this = this;
 
 getCourseId = async function (ids) {
-  let coursesId = [];
+  let coursesIds = [];
   ids.forEach(async (courseId) => {
-      if(courseId === ""){}
-      else {
+      if (courseId != '') {
         let course = await Course.findOne({courseNumber: courseId});
-        coursesId.push(course._id);
+        coursesIds.push(course._id);
       }
     }
   );
-  return coursesId;
+  return coursesIds;
 };
 
 getQueryRegex = function (query) {
@@ -24,15 +24,31 @@ getQueryRegex = function (query) {
 }
 
 
+getCourseNumbersFromList = function(list) {
+  const res = [];
+  if (list === undefined || list.length == 0) {
+    return res;
+  }
+  list.forEach(function (listItem) {
+    const num = listItem.courseNumber;
+    res.push(num);
+  });
+  return res;
+};
+
 
 exports.getQuestionsFromQuery = async function(query) {
  if(!query.content){
    return [];
  }
   let questions = [];
-  let coursesId = [];
-  if(query.filters) {
-    coursesId = await getCourseId(query.filters).then();
+  let courses = [];
+  if(query.filters && !Array.isArray(query.filters)) {
+    courses = query.filters.split(',');
+    //coursesId = await getCourseId(query.filters)
+  }
+  else {
+    courses = query.filters;
   }
 
   const queryRegex = getQueryRegex(query.content);
@@ -41,15 +57,15 @@ exports.getQuestionsFromQuery = async function(query) {
       {subject: {$regex: queryRegex, $options: 'si'}},
       {content: {$regex: queryRegex, $options: 'si'}}
     ]
-  }).catch(e => console.log(e));
+  }).populate([{ path: 'relatedCourses' }]);
 
   if (query.filters) {
-    coursesId.forEach((id) => {
-      tmpQuestions.forEach( (question) => {
-        if (question.relatedCourses.indexOf(id) > -1) {
-          questions.push(question)
-        }
-      });
+    tmpQuestions.forEach( (question) => {
+      let inFilter = courses.some(r => getCourseNumbersFromList(question.relatedCourses).includes(r));
+      if (inFilter) {
+        questions.push(question);
+        dbIds.push(question._id);
+      }
     });
   } else {
     questions = tmpQuestions;
