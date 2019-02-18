ebaa1d8c6f04ec6e50d5fda622bbf42c37db5a74
OR <OR@PC>
2019-02-18 17:33:24 +0200

    changes for prod

--- src/app/services/http-requests.service.ts
+++ src/app/services/http-requests.service.ts
@@ -18,7 +18,7 @@ export class QueryParams {
 @Injectable()
 export class HttpRequestsService {
 
-  private _baseUrl = 'http://localhost:3000/api';
+  private _baseUrl = 'https://d1dl8vvg5ldc6n.cloudfront.net/api';
 
   constructor(private http: HttpClient) {
     // this.http.post(this._baseUrl + '/courses', new UiCourse('blah', '0011'));
