adea017498d76f921658cdfcb631c639ec6411b6
OR <OR@PC>
2019-02-21 10:03:25 +0200

    delete TODOs

--- src/app/services/program.service.ts
+++ src/app/services/program.service.ts
@@ -10,20 +10,7 @@ export class ProgramService {
   private _programs: Program[] = [];
   private _programsLoaded: Promise<void>;
 
-  // // TODO(or, nati): Add programs to DB and implement the BE.
-  // private _dummyPrograms: string[] = ['Computer Science', 'Electrical Engineering',
-  //   'Law', 'Computer Science and Electrical Engineering', 'Economics',
-  //   'Management', 'Physics', 'Chemistry'];
-  // private _implemented = false;
-  // // end of TODO
-
   constructor(private httpRequest: HttpRequestsService) {
-    // // TODO(or, nati): Remove after implemented.
-    // if (!this._implemented) {
-    //   this._dummyPrograms.forEach((name) => this._programs.push(new Program('0', name)));
-    //   return;
-    // }
-    // // end of TODO
     this._programsLoaded = new Promise<void>((resolve) => {
       this.httpRequest.get('/programs').subscribe((programs: any) => {
         programs.forEach((program) => {
@@ -35,11 +22,6 @@ export class ProgramService {
   }
 
   getPrograms(): Promise<Program[]> {
-    // // TODO(or, nati): Remove after implemented.
-    // if (!this._implemented) {
-    //   return Promise.resolve(this._programs);
-    // }
-    // // end of TODO
     return new Promise<Program[]>(async resolve => {
       await this._programsLoaded;
       resolve(this._programs);
